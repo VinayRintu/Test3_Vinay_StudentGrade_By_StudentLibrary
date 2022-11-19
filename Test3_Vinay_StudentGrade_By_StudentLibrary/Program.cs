9d13144dd8c2e879bc56b7f0e1056867c5c8f10a
Rintu <Rintu@VINDUJA>
2022-11-19 13:11:09 +0530

    Add project files.

---
+++ Test3_Vinay_StudentGrade_By_StudentLibrary/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test3_Vinay_StudentGrade_By_StudentLibrary;
+
+Console.WriteLine("Hello, World!");
+
+Student_Grade objectStudentGrade = new Student_Grade();
+objectStudentGrade.CalculateGrade();
